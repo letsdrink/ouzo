f842973b35be014a326a7549ec6256ffbe489904
christopherch <krzykulec@gmail.com>
2013-08-28 12:28:42 +0200

    shouldRedirectToOldPanelTest()

--- test/lib/Thulium/ControllerTest.php
+++ test/lib/Thulium/ControllerTest.php
@@ -6,6 +6,15 @@ class SimpleTestController extends Controller
 {
 }
 
+class SampleConfig
+{
+    public function getConfig()
+    {
+        $config['global']['prefix_system'] = '/path/to/panel/';
+        return $config;
+    }
+}
+
 class ControllerTest extends PHPUnit_Framework_TestCase
 {
     /**
@@ -18,5 +27,23 @@ class ControllerTest extends PHPUnit_Framework_TestCase
 
         //then
         $this->assertEquals('simple_test', $tab);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldRedirectToOldPanel()
+    {
+        //given
+        Thulium\Config::registerConfig(new SampleConfig);
+        $controller = new Controller();
+
+        //when
+        $controller->redirectOld('test.php', array('param1' => 'abc', 'param2' => 'def'));
+        $redirect_location =  $controller->getRedirectLocation();
+
+        //then
+        $this->assertEquals('/path/to/panel/test.php?param1=abc&param2=def', $redirect_location);
+
     }
 }
