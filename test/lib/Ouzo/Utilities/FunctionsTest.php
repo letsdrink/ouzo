51d947cf5c9a11cbb3c8950c5a4a38f3dcd97660
woru <wor18@wp.pl>
2013-09-25 12:30:31 +0200

    cleanup & tests

---
+++ test/lib/Ouzo/Utilities/FunctionsTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Ouzo\Utilities;
+
+
+use Model\Product;
+
+class FunctionsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldExtractId()
+    {
+        //given
+        $product = new Product(array('id_product' => 1));
+
+        //when
+        $id = Functions::call(Functions::extractId(), $product);
+
+        //then
+        $this->assertEquals(1, $id);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldTrimWhiteCharacters()
+    {
+        //given
+        $string = " snow ";
+
+        //when
+        $trimmed = Functions::call(Functions::trim(), $string);
+
+        //then
+        $this->assertEquals('snow', $trimmed);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNegatePredicate()
+    {
+        $this->assertFalse(Functions::call(Functions::not(Functions::identity()), true));
+        $this->assertTrue(Functions::call(Functions::not(Functions::identity()), false));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldTestIfArgumentIsArray()
+    {
+        $this->assertFalse(Functions::call(Functions::isArray(), 'string'));
+        $this->assertTrue(Functions::call(Functions::isArray(), array()));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldPrependPrefixToArgument()
+    {
+        //given
+        $string = "snow";
+
+        //when
+        $prefixed = Functions::call(Functions::prepend('white '), $string);
+
+        //then
+        $this->assertEquals('white snow', $prefixed);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAppendPostfixToArgument()
+    {
+        //given
+        $string = "white";
+
+        //when
+        $modified = Functions::call(Functions::append(' snow'), $string);
+
+        //then
+        $this->assertEquals('white snow', $modified);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldComposeFunctions()
+    {
+        //given
+        $functionA = Functions::trim();
+        $functionB = Functions::append('a ');
+
+        //when
+        $result = Functions::call(Functions::compose($functionA, $functionB), ' ');
+
+        //then
+        $this->assertEquals('a', $result);
+    }
+}
