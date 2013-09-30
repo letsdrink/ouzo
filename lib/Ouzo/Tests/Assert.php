aa56bacae5c7a7ba24301d97c5bf5ea96b39eb3c
piotrooo <piotroo89@gmail.com>
2013-09-30 09:02:57 +0200

    Add docs to Assert.

--- lib/Ouzo/Tests/Assert.php
+++ lib/Ouzo/Tests/Assert.php
@@ -10,6 +10,7 @@ namespace Ouzo\Tests;
  *  Assert::thatArray($animals)->hasSize(3)->contains('cat');
  *  Assert::thatArray($animals)->containsOnly('pig', 'dog', 'cat');
  *  Assert::thatArray($animals)->containsExactly('cat', 'dog', 'pig');
+ *  Assert::thatArray(array('id' => 123, 'name' => 'john'))->containsKeyAndValue(array('id' => 123));
  * </code>
  */
 class Assert
