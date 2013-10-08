3a1efc658463734b95a97d7fc8b62799472b1e2b
piotrooo <piotroo89@gmail.com>
2013-10-08 15:22:14 +0200

    Add phpDoc to Arrays.

--- lib/Ouzo/Utilities/Arrays.php
+++ lib/Ouzo/Utilities/Arrays.php
@@ -1,8 +1,29 @@
 <?php
 namespace Ouzo\Utilities;
 
+use InvalidArgumentException;
+
+/**
+ * Class Arrays
+ * @package Ouzo\Utilities
+ */
 class Arrays
 {
+    /**
+     * Returns true if every element in array satisfies the predicate.
+     *
+     * Example:
+     * <code>
+     * $array = array(1, 2);
+     * $all = Arrays::all($array, function ($element) {
+     *      return $element < 3;
+     * });
+     * </code>
+     *
+     * @param array $elements
+     * @param $predicate
+     * @return bool
+     */
     public static function all(array $elements, $predicate)
     {
         foreach ($elements as $element) {
@@ -13,6 +34,24 @@ class Arrays
         return true;
     }
 
+    /**
+     * This method creates associative array using key and value functions on array elements.
+     *
+     * Example:
+     * <code>
+     * $array = range(1, 2);
+     * $map = Arrays::toMap($array, function ($elem) {
+     *      return $elem * 10;
+     * }, function ($elem) {
+     *      return $elem + 1;
+     * });
+     * </code>
+     *
+     * @param array $elements
+     * @param $keyFunction
+     * @param null $valueFunction
+     * @return array
+     */
     public static function toMap(array $elements, $keyFunction, $valueFunction = null)
     {
         if ($valueFunction == null) {
@@ -24,6 +63,28 @@ class Arrays
         return empty($keys) ? array() : array_combine($keys, $values);
     }
 
+    /**
+     * Returns a new array that is a one-dimensional flattening of the given array.
+     *
+     * Example:
+     * <code>
+     * $array = array(
+     *      'names' => array(
+     *          'john',
+     *          'peter',
+     *          'bill'
+     *      ),
+     *      'products' => array(
+     *          'cheese',
+     *          array('milk', 'brie')
+     *      )
+     * );
+     * $flatten = Arrays::flatten($array);
+     * </code>
+     *
+     * @param array $elements
+     * @return array
+     */
     static function flatten(array $elements)
     {
         $return = array();
@@ -33,6 +94,24 @@ class Arrays
         return $return;
     }
 
+    /**
+     * This method returns a key for the given value.
+     *
+     * Example:
+     * <code>
+     * $array = array(
+     *      'k1' => 4,
+     *      'k2' => 'd',
+     *      'k3' => 0,
+     *      9 => 'p'
+     * );
+     * $key = Arrays::findKeyByValue($array, 0);
+     * </code>
+     *
+     * @param array $elements
+     * @param $value
+     * @return bool|int|string
+     */
     static public function findKeyByValue(array $elements, $value)
     {
         if ($value === 0) {
@@ -46,6 +125,21 @@ class Arrays
         return FALSE;
     }
 
+    /**
+     * Returns true if at least one element in the array satisfies the predicate.
+     *
+     * Example:
+     * <code>
+     * $array = array('a', true, 'c');
+     * $any = Arrays::any($array, function ($element) {
+     *      return is_bool($element);
+     * });
+     * </code>
+     *
+     * @param array $elements
+     * @param $predicate
+     * @return bool
+     */
     public static function any(array $elements, $predicate)
     {
         foreach ($elements as $element) {
@@ -56,44 +150,156 @@ class Arrays
         return false;
     }
 
+    /**
+     * This method returns the first value in the given array.
+     *
+     * Example:
+     * <code>
+     * $array = array('one', 'two' 'three');
+     * $first = Arrays::first($array);
+     * </code>
+     *
+     * @param array $elements
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
     public static function first(array $elements)
     {
         if (empty($elements)) {
-            throw new \InvalidArgumentException('empty array');
+            throw new InvalidArgumentException('empty array');
         }
         $keys = array_keys($elements);
         return $elements[$keys[0]];
     }
 
+    /**
+     * This method returns the last value in the given array.
+     *
+     * Example:
+     * <code>
+     * $array = array('a', 'b', 'c');
+     * $last = Arrays::last($array);
+     * </code>
+     *
+     * @param array $elements
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
     public static function last(array $elements)
     {
         if (empty($elements)) {
-            throw new \InvalidArgumentException('empty array');
+            throw new InvalidArgumentException('empty array');
         }
         return end($elements);
     }
 
+    /**
+     * This method returns the first value or null if array is empty.
+     *
+     * Example:
+     * <code>
+     * $array = array();
+     * $return = Arrays::firstOrNull($array);
+     * </code>
+     *
+     * @param array $elements
+     * @return mixed|null
+     */
     public static function firstOrNull(array $elements)
     {
         return empty($elements) ? null : self::first($elements);
     }
 
+    /**
+     * Returns the element for the given key or a default value otherwise.
+     *
+     * Example:
+     * <code>
+     * $array = array('id' => 1, 'name' => 'john');
+     * $value = Arrays::getValue($array, 'name');
+     * </code>
+     *
+     * Example:
+     * <code>
+     * $array = array('id' => 1, 'name' => 'john');
+     * $value = Arrays::getValue($array, 'surname', '--not found--');
+     * </code>
+     *
+     * @param array $elements
+     * @param $key
+     * @param null $default
+     * @return null
+     */
     public static function getValue(array $elements, $key, $default = null)
     {
         return isset($elements[$key]) ? $elements[$key] : $default;
     }
 
+    /**
+     * Returns an array containing only the given keys.
+     *
+     * Example:
+     * <code>
+     * $array = array('a' => 1, 'b' => 2, 'c' => 3);
+     * $filtered = Arrays::filterByAllowedKeys($array, array('a', 'b'));
+     * </code>
+     *
+     * @param array $elements
+     * @param $allowedKeys
+     * @return array
+     */
     public static function filterByAllowedKeys(array $elements, $allowedKeys)
     {
         return array_intersect_key($elements, array_flip($allowedKeys));
     }
 
+    /**
+     * Filters array by keys using the predicate.
+     *
+     * Example:
+     * <code>
+     * $array = array('a1' => 1, 'a2' => 2, 'c' => 3);
+     * $filtered = Arrays::filterByKeys($array, function ($elem) {
+     *      return $elem[0] == 'a';
+     * });
+     * </code>
+     *
+     * @param array $elements
+     * @param $predicate
+     * @return array
+     */
     public static function filterByKeys(array $elements, $predicate)
     {
         $allowedKeys = array_filter(array_keys($elements), $predicate);
         return self::filterByAllowedKeys($elements, $allowedKeys);
     }
 
+    /**
+     * Group elements in array using function to grouping elements. If set $orderField grouped elements will be also sorted.
+     *
+     * Example:
+     * <code>
+     * $obj1 = new stdClass();
+     * $obj1->name = 'a';
+     * $obj1->description = '1';
+     *
+     * $obj2 = new stdClass();
+     * $obj2->name = 'b';
+     * $obj2->description = '2';
+     *
+     * $obj3 = new stdClass();
+     * $obj3->name = 'b';
+     * $obj3->description = '3';
+     *
+     * $array = array($obj1, $obj2, $obj3);
+     * $grouped = Arrays::groupBy($array, Functions::extractField('name'));
+     * </code>
+     *
+     * @param array $elements
+     * @param $keyFunction
+     * @param null $orderField
+     * @return array
+     */
     public static function groupBy(array $elements, $keyFunction, $orderField = null)
     {
         $map = array();
@@ -107,6 +313,31 @@ class Arrays
         return $map;
     }
 
+    /**
+     * This method sorts elements in array using order field.
+     *
+     * Example:
+     * <code>
+     * $obj1 = new stdClass();
+     * $obj1->name = 'a';
+     * $obj1->description = '1';
+     *
+     * $obj2 = new stdClass();
+     * $obj2->name = 'c';
+     * $obj2->description = '2';
+     *
+     * $obj3 = new stdClass();
+     * $obj3->name = 'b';
+     * $obj3->description = '3';
+     *
+     * $array = array($obj1, $obj2, $obj3);
+     * $sorted = Arrays::orderBy($array, 'name');
+     * </code>
+     *
+     * @param array $elements
+     * @param $orderField
+     * @return array
+     */
     public static function orderBy(array $elements, $orderField)
     {
         usort($elements, function ($a, $b) use ($orderField) {
@@ -115,6 +346,26 @@ class Arrays
         return $elements;
     }
 
+    /**
+     * This method maps array keys using the function.
+     * Invokes the function for each key in the array. Creates a new array containing the keys returned by the function.
+     *
+     * Example:
+     * <code>
+     * $array = array(
+     *      'k1' => 'v1',
+     *      'k2' => 'v2',
+     *      'k3' => 'v3'
+     * );
+     * $arrayWithNewKeys = Arrays::mapKeys($array, function ($key) {
+     *      return 'new_' . $key;
+     * });
+     * </code>
+     *
+     * @param array $elements
+     * @param $function
+     * @return array
+     */
     public static function mapKeys(array $elements, $function)
     {
         $newArray = array();
@@ -125,26 +376,94 @@ class Arrays
         return $newArray;
     }
 
+    /**
+     * This method maps array values using the function.
+     * Invokes the function for each value in the array. Creates a new array containing the values returned by the function.
+     *
+     * Example:
+     * <code>
+     * $array = array('k1', 'k2', 'k3');
+     * $result = Arrays::map($array, function ($value) {
+     *      return 'new_' . $value;
+     * });
+     * </code>
+     *
+     * @param array $elements
+     * @param $function
+     * @return array
+     */
     public static function map(array $elements, $function)
     {
         return array_map($function, $elements);
     }
 
+    /**
+     * This method filters array using function. Result contains all elements for which function returns true.
+     *
+     * Example:
+     * <code>
+     * $array = array(1, 2, 3, 4);
+     * $result = Arrays::filter($array, function ($value) {
+     *      return $value > 2;
+     * });
+     * </code>
+     *
+     * @param array $elements
+     * @param $function
+     * @return array
+     */
     public static function filter(array $elements, $function)
     {
         return array_filter($elements, $function);
     }
 
+    /**
+     * Make array from element. Returns the given argument if it's already an array.
+     *
+     * Example:
+     * <code>
+     * $result = Arrays::toArray('test');
+     * </code>
+     *
+     * @param $element
+     * @return array
+     */
     public static function toArray($element)
     {
         return $element ? is_array($element) ? $element : array($element) : array();
     }
 
+    /**
+     * Returns a random element from the given array.
+     *
+     * Example:
+     * <code>
+     * $array = array('john', 'city', 'small');
+     * $rand = Arrays::randElement($array);
+     * </code>
+     *
+     * @param $elements
+     * @return null
+     */
     public static function randElement($elements)
     {
         return $elements ? $elements[array_rand($elements)] : null;
     }
 
+    /**
+     * Returns a new array with $keys as array keys and $values as array values.
+     *
+     * Example:
+     * <code>
+     * $keys = array('id', 'name', 'surname');
+     * $values = array(1, 'john', 'smith');
+     * $combined = Arrays::combine($keys, $values);
+     * </code>
+     *
+     * @param array $keys
+     * @param array $values
+     * @return array
+     */
     public static function combine(array $keys, array $values)
     {
         if (!empty($keys) && !empty($values)) {
@@ -153,6 +472,19 @@ class Arrays
         return array();
     }
 
+    /**
+     * Checks is key exists in an array.
+     *
+     * Example:
+     * <code>
+     * $array = array('id' => 1, 'name' => 'john');
+     * $return = Arrays::keyExists($array, 'name');
+     * </code>
+     *
+     * @param array $elements
+     * @param $key
+     * @return bool
+     */
     public static function keyExists(array $elements, $key)
     {
         return array_key_exists($key, $elements);
