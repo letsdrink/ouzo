817effbffc10defc7ed1d63553c2613cf26abafe
piotrooo <piotroo89@gmail.com>
2013-10-29 13:36:54 +0100

    Add test to download file. Cleanup.

--- test/src/Ouzo/ControllerTest.php
+++ test/src/Ouzo/ControllerTest.php
@@ -1,15 +1,26 @@
 <?php
-
-use Ouzo\Config;
 use Ouzo\Controller;
+use Ouzo\ControllerFactory;
+use Ouzo\Routing\Route;
+use Ouzo\Tests\ControllerTestCase;
 
 class SimpleTestController extends Controller
 {
+    public function download()
+    {
+        $this->downloadFile('file.txt', 'text/plain', '/tmp/file.txt');
+    }
 }
 
-
-class ControllerTest extends PHPUnit_Framework_TestCase
+class ControllerTest extends ControllerTestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_frontController->controllerFactory = new ControllerFactory('\\');
+        Route::$routes = array();
+    }
+
     /**
      * @test
      */
@@ -22,4 +33,19 @@ class ControllerTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('simple_test', $tab);
     }
 
+    /**
+     * @test
+     * @covers \Ouzo\DownloadHandler
+     */
+    public function shouldDownloadFile()
+    {
+        //given
+        Route::get('/simple_test/download', 'simple_test#download');
+
+        //when
+        $this->get('/simple_test/download');
+
+        //then
+        $this->assertDownloadFile('file.txt');
+    }
 }
