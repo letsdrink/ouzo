4e4fb70a32d88e0560f4237da9c2d62328627a36
woru <wor18@wp.pl>
2013-11-15 11:02:28 +0100

    added mock

---
+++ src/Ouzo/Tests/Mock.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Ouzo\Tests;
+
+use Ouzo\Utilities\Arrays;
+use Ouzo\Utilities\Joiner;
+use PHPUnit_Framework_ComparisonFailure;
+use PHPUnit_Framework_ExpectationFailedException;
+
+class CallStub
+{
+    public $methodCall;
+    public $result;
+
+    function __construct($methodCall, $result)
+    {
+        $this->methodCall = $methodCall;
+        $this->result = $result;
+    }
+}
+
+class MethodCall
+{
+    public $name;
+    public $arguments;
+
+    function __construct($name, $arguments)
+    {
+        $this->name = $name;
+        $this->arguments = $arguments;
+    }
+
+    public function toString()
+    {
+        return $this->name . '(' . Joiner::on(', ')->join($this->arguments) . ')';
+    }
+
+    public static function create($name, $arguments)
+    {
+        return new MethodCall($name, $arguments);
+    }
+
+    public static function hasName($name)
+    {
+        return function ($callStub) use ($name) {
+            return $callStub->name == $name;
+        };
+    }
+
+    public static function toStringFunction()
+    {
+        return function (MethodCall $methodCall) {
+            return $methodCall->toString();
+        };
+    }
+}
+
+class WhenBuilder
+{
+    private $mock;
+    private $methodCall;
+
+    function __construct(Mock $mock)
+    {
+        $this->mock = $mock;
+    }
+
+    function __call($name, $arguments)
+    {
+        $this->methodCall = new MethodCall($name, $arguments);
+        return $this;
+    }
+
+    function thenReturn($result)
+    {
+        $this->mock->_stubbed_calls[] = new CallStub($this->methodCall, $result);
+    }
+}
+
+class Verifier
+{
+    private $mock;
+
+    function __construct($mock)
+    {
+        $this->mock = $mock;
+    }
+
+    function __call($name, $arguments)
+    {
+        foreach ($this->mock->_called_methods as $called_method) {
+            if ($called_method->name == $name && $called_method->arguments === $arguments) {
+                return $this;
+            }
+        }
+        $calls = $this->actualCalls();
+        $expected = MethodCall::create($name, $arguments)->toString();
+        $this->fail("Expected method was not called", $expected, $calls);
+    }
+
+    private function fail($description, $expected, $actual)
+    {
+        throw new PHPUnit_Framework_ExpectationFailedException(
+            $description,
+            new PHPUnit_Framework_ComparisonFailure($expected, $actual, $expected, $actual)
+        );
+    }
+
+    private function actualCalls()
+    {
+        if (empty($this->mock->_called_methods)) {
+            return "no interactions";
+        }
+        return Joiner::on(', ')->join(Arrays::map($this->mock->_called_methods, MethodCall::toStringFunction()));
+    }
+
+}
+
+class Mock
+{
+    public $_stubbed_calls = array();
+    public $_called_methods = array();
+
+    public static function mock()
+    {
+        return new Mock();
+    }
+
+    public static function when(Mock $mock)
+    {
+        return new WhenBuilder($mock);
+    }
+
+    public static function verify($mock)
+    {
+        return new Verifier($mock);
+    }
+
+    function __call($name, $arguments)
+    {
+        $methodCall = new MethodCall($name, $arguments);
+        $this->_called_methods[] = $methodCall;
+
+        foreach ($this->_stubbed_calls as $stubbed_call) {
+            if ($stubbed_call->methodCall == $methodCall) {
+                return $stubbed_call->result;
+            }
+        }
+        return null;
+    }
+} 
