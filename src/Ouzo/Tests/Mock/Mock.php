ea32b7e9e1159cdb23164af486e9625e56b5c263
woru <wor18@wp.pl>
2013-11-25 12:06:20 +0100

    cleanup

--- src/Ouzo/Tests/Mock/Mock.php
+++ src/Ouzo/Tests/Mock/Mock.php
@@ -4,25 +4,6 @@ namespace Ouzo\Tests\Mock;
 
 use Ouzo\Utilities\DynamicProxy;
 
-class SimpleMock
-{
-    public $_stubbed_calls = array();
-    public $_called_methods = array();
-
-    function __call($name, $arguments)
-    {
-        $methodCall = new MethodCall($name, $arguments);
-        $this->_called_methods[] = $methodCall;
-
-        foreach ($this->_stubbed_calls as $stubbed_call) {
-            if ($stubbed_call->matches($methodCall)) {
-                return $stubbed_call->evaluate();
-            }
-        }
-        return null;
-    }
-}
-
 class Mock
 {
     public static function mock($className = null)
