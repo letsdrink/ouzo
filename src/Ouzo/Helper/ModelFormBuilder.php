50ee8f7cc8be42e27648f75812491da1ba3896fb
woru <wor18@wp.pl>
2014-01-10 15:29:41 +0100

    model form builder: add error class to invalid fields

--- src/Ouzo/Helper/ModelFormBuilder.php
+++ src/Ouzo/Helper/ModelFormBuilder.php
@@ -2,6 +2,7 @@
 namespace Ouzo\Helper;
 
 use Ouzo\I18n;
+use Ouzo\Utilities\Joiner;
 
 class ModelFormBuilder
 {
@@ -32,6 +33,10 @@ class ModelFormBuilder
         $id = $this->_generateId($field);
         $name = $this->_generateName($field);
         $attributes = array('id' => $id, 'name' => $name);
+
+        if (in_array($field, $this->_object->getErrorFields())) {
+            $attributes['class'] = 'error';
+        }
         return $attributes;
     }
 
@@ -48,42 +53,42 @@ class ModelFormBuilder
     public function textField($field, array $options = array())
     {
         $attributes = $this->_generatePredefinedAttributes($field);
-        $attributes = array_merge($attributes, $options);
+        $attributes = $this->_mergeAttributes($attributes, $options);
         return textFieldTag($this->_object->$field, $attributes);
     }
 
     public function textArea($field, array $options = array())
     {
         $attributes = $this->_generatePredefinedAttributes($field);
-        $attributes = array_merge($attributes, $options);
+        $attributes = $this->_mergeAttributes($attributes, $options);
         return textAreaTag($this->_object->$field, $attributes);
     }
 
     public function selectField($field, array $items, $options = array(), $defaultOption = null)
     {
         $attributes = $this->_generatePredefinedAttributes($field);
-        $attributes = array_merge($attributes, $options);
+        $attributes = $this->_mergeAttributes($attributes, $options);
         return selectTag($items, array($this->_object->$field), $attributes, $defaultOption);
     }
 
     public function hiddenField($field, $value = null, $options = array())
     {
         $attributes = $this->_generatePredefinedAttributes($field);
-        $attributes = array_merge($attributes, $options);
+        $attributes = $this->_mergeAttributes($attributes, $options);
         return hiddenTag($value ? $value : $this->_object->$field, $attributes);
     }
 
     public function passwordField($field, array $options = array())
     {
         $attributes = $this->_generatePredefinedAttributes($field);
-        $attributes = array_merge($attributes, $options);
+        $attributes = $this->_mergeAttributes($attributes, $options);
         return passwordFieldTag($this->_object->$field, $attributes);
     }
 
     public function checkboxField($field, array $options = array())
     {
         $attributes = $this->_generatePredefinedAttributes($field);
-        $attributes = array_merge($attributes, $options);
+        $attributes = $this->_mergeAttributes($attributes, $options);
         $value = $this->_object->$field;
         $checked = !empty($value);
         return checkboxTag('1', $checked, $attributes);
@@ -102,5 +107,13 @@ class ModelFormBuilder
     public function getObject()
     {
         return $this->_object;
+    }
+
+    private function _mergeAttributes($attributes, $options)
+    {
+        if (isset($options['class'])) {
+            $options['class'] = Joiner::on(' ')->skipNulls()->join(array($options['class'], $attributes['class']));
+        }
+        return array_merge($attributes, $options);
     }
 }
---
+++ test/src/Ouzo/Helper/ModelFormBuilderTest.php
@@ -0,0 +1,66 @@
+<?php
+
+use Model\Test\Product;
+use Ouzo\Helper\ModelFormBuilder;
+use Ouzo\Tests\DbTransactionalTestCase;
+use Ouzo\View;
+
+
+class ModelFormBuilderTest extends DbTransactionalTestCase
+{
+    function setUp()
+    {
+        parent::setUp();
+        new View('test');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAddErrorClass()
+    {
+        //given
+        $product = new Product(array());
+        $product->validate();
+        $formBuilder = new ModelFormBuilder($product);
+
+        //when
+        $html = $formBuilder->textField('name');
+
+        //then
+        $this->assertContains('class="error"', $html);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAddErrorClassIfClassGiven()
+    {
+        //given
+        $product = new Product(array());
+        $product->validate();
+        $formBuilder = new ModelFormBuilder($product);
+
+        //when
+        $html = $formBuilder->textField('name', array('class' => 'class1 class2'));
+
+        //then
+        $this->assertContains('class="class1 class2 error"', $html);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotAddEmptyClassAttribute()
+    {
+        //given
+        $product = new Product(array('name' => 'valid'));
+        $formBuilder = new ModelFormBuilder($product);
+
+        //when
+        $html = $formBuilder->textField('name');
+
+        //then
+        $this->assertNotContains('class="', $html);
+    }
+} 
