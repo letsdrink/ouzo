b31ab9c9a52fb6851fe9d774ac6f2b5482342aab
andrzejo <andrzej.oczkowicz@gmail.com>
2014-02-26 12:20:52 +0100

    Shell dispatcher: error handling enhancement.

--- test/src/Ouzo/Shell/DispatcherTest.php
+++ test/src/Ouzo/Shell/DispatcherTest.php
@@ -1,7 +1,8 @@
 <?php
-use Ouzo\Shell;
 use Ouzo\Shell\Dispatcher;
+use Ouzo\Shell;
 use Ouzo\Shell\InputArgument;
+use Ouzo\Tests\CatchException;
 use Ouzo\Tests\MockShellOutput;
 
 class SampleShell extends Shell
@@ -21,6 +22,11 @@ class SampleShell extends Shell
         $this->out('SAMPLE CONSOLE');
         $this->out('ARG:' . $this->getArgument('test'));
     }
+}
+
+class SampleClassButNotExtendsShell
+{
+
 }
 
 class DispatcherTest extends PHPUnit_Framework_TestCase
@@ -53,5 +59,70 @@ class DispatcherTest extends PHPUnit_Framework_TestCase
 
         //then
         $this->expectOutputRegex('/ARG:John/');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrowExceptionIfAppIsNotSpecified()
+    {
+        //given
+        $argv = array('shell.php');
+
+        //when
+        CatchException::when($this)->_invokeDispatcher($argv);
+
+        //then
+        CatchException::assertThat()->isInstanceOf('Ouzo\Shell\DispatcherException');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldEchoMessageFromUserException()
+    {
+        //given
+        $argv = array('shell.php');
+
+        //when
+        Dispatcher::runScript($argv);
+
+        //then
+        $this->expectOutputRegex('/Error: Empty application name./');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldEchoErrorIfApplicationNotFoundOr()
+    {
+        //given
+        $argv = array('shell.php', 'NotExist');
+
+        //when
+        Dispatcher::runScript($argv);
+
+        //then
+        $this->expectOutputRegex("/Class 'NotExistShell' for application 'NotExist' does not exist./");
+    }
+
+    /**
+     * @test
+     */
+    public function shouldEchoErrorIfApplicationNotShell()
+    {
+        //given
+        $argv = array('shell.php', 'SampleClassButNotExtends');
+
+        //when
+        Dispatcher::runScript($argv);
+
+        //then
+        $this->expectOutputRegex("/Error: Class 'SampleClassButNotExtendsShell' is not shell application./");
+    }
+
+    public function _invokeDispatcher($argv)
+    {
+        Dispatcher::runScript($argv);
     }
 }
