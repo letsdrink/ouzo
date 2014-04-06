2c0909537ff8e34395ef2ca89849f62a480f32b4
woru <wor18@wp.pl>
2014-04-06 18:07:02 +0200

    added PreparedStatementEmulator

---
+++ src/Ouzo/Db/PreparedStatementEmulator.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Ouzo\Db;
+
+use Ouzo\Utilities\Objects;
+
+class ParameterPlaceHolderCallback
+{
+    private $_boundValues = array();
+    private $_param_index = 0;
+    private $_quote_count = 0;
+
+    function __construct($boundValues)
+    {
+        $this->_boundValues = $boundValues;
+    }
+
+    function __invoke($matches)
+    {
+        if ($matches[0] == "'") {
+            $this->_quote_count++;
+            return "'";
+        } else if (!$this->isInString()) {
+            return $this->substituteParam();
+        } else {
+            return "?";
+        }
+    }
+
+    private function substituteParam()
+    {
+        $value = $this->_boundValues[$this->_param_index];
+        $this->_param_index++;
+        if ($value === null) {
+            return "null";
+        }
+        if (is_bool($value)) {
+            return Objects::booleanToString($value);
+        }
+        return "'$value'";
+    }
+
+    private function isInString()
+    {
+        return $this->_quote_count % 2 == 1;
+    }
+}
+
+class PreparedStatementEmulator {
+
+    public static function substitute($sql, $params)
+    {
+        return preg_replace_callback('/[\'?]/', new ParameterPlaceHolderCallback($params), $sql);
+    }
+}
---
+++ test/src/Ouzo/Db/PreparedStatementEmulatorTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Ouzo\Db;
+
+
+use Ouzo\Tests\Assert;
+
+class PreparedStatementEmulatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldSubstituteParametersInSimpleQuery()
+    {
+        //given
+        $sql = 'select * from users where name = ? and surname = ?';
+        $params = array('bob', 'smith');
+
+        //when
+        $result = PreparedStatementEmulator::substitute($sql, $params);
+
+        //then
+        Assert::thatString($result)->isEqualTo("select * from users where name = 'bob' and surname = 'smith'");
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSubstituteBooleanParameters()
+    {
+        //given
+        $sql = 'select * from users where verified = ? or admin = ?';
+        $params = array(true, false);
+
+        //when
+        $result = PreparedStatementEmulator::substitute($sql, $params);
+
+        //then
+        Assert::thatString($result)->isEqualTo("select * from users where verified = true or admin = false");
+    }
+
+    /**
+     * @test
+     */
+    public function shouldConvertNumbersToStrings()
+    {
+        //given
+        $sql = "select * from users where age = ?";
+        $params = array('26');
+
+        //when
+        $result = PreparedStatementEmulator::substitute($sql, $params);
+
+        //then
+        Assert::thatString($result)->isEqualTo("select * from users where age = '26'");
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotSubstituteQuestionMarksInStrings()
+    {
+        //given
+        $sql = "select * from users where id = '?'";
+        $params = array();
+
+        //when
+        $result = PreparedStatementEmulator::substitute($sql, $params);
+
+        //then
+        Assert::thatString($result)->isEqualTo($sql);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSubstituteParametersWhenSqlContainsStrings()
+    {
+        //given
+        $sql = "select * from users where name = 'bob?' and age = ? and  surname = 'smith?'";
+        $params = array('26');
+
+        //when
+        $result = PreparedStatementEmulator::substitute($sql, $params);
+
+        //then
+        Assert::thatString($result)->isEqualTo("select * from users where name = 'bob?' and age = '26' and  surname = 'smith?'");
+    }
+}
+ 
