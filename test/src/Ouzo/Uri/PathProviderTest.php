0c3eefea48974c44874354959bc26c365312a0dc
Bartosz Bańkowski <bbankowski@gmail.com>
2014-05-06 12:49:01 +0200

    Added test case for path provider.

---
+++ test/src/Ouzo/Uri/PathProviderTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Ouzo\Uri;
+
+use PHPUnit_Framework_TestCase;
+
+class PathProviderTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function shouldReturnRedirectUrlIfExists()
+    {
+        // given
+        $provider = new PathProvider();
+        $_SERVER['REDIRECT_URL'] = '/redirect/url';
+
+        // when
+        $path = $provider->getPath();
+
+        // then
+        $this->assertEquals('/redirect/url', $path);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnRedirectUrlWithRedirectQueryStringIfExists()
+    {
+        // given
+        $provider = new PathProvider();
+        $_SERVER['REDIRECT_URL'] = '/redirect/url';
+        $_SERVER['REDIRECT_QUERY_STRING'] = 'id=1&name=john';
+
+        // when
+        $path = $provider->getPath();
+
+        // then
+        $this->assertEquals('/redirect/url?id=1&name=john', $path);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnRequestUriIfRedirectUrlNotExist()
+    {
+        // given
+        $provider = new PathProvider();
+        $_SERVER['REQUEST_URI'] = '/request/uri';
+
+        // when
+        $path = $provider->getPath();
+
+        // then
+        $this->assertEquals('/request/uri', $path);
+    }
+
+}
+ 
