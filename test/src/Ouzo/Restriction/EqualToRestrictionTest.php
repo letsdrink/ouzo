470e94c1823198e878676e33ab6c322df07fc058
bbankowski <bbankowski@gmail.com>
2014-11-11 18:18:22 +0100

    Added missing test case for equal to restriction.

---
+++ test/src/Ouzo/Restriction/EqualToRestrictionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Ouzo\Restrictions;
+
+class EqualToRestrictionTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function shouldAcceptRestrictionsAsValueInWhere()
+    {
+        //given
+        $restriction = Restrictions::equalTo('value');
+
+        //when
+        $sql = $restriction->toSql('key');
+
+        //then
+        $this->assertEquals('key = ?', $sql);
+        $this->assertEquals('value', $restriction->getValues());
+    }
+}
+ 
