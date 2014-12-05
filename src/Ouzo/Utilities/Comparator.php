3a710bf0db45d4ea8690db78fd81ac483115b53d
Michal Zmuda <zmudamichal92@gmail.com>
2014-12-05 13:29:21 +0100

    Sorting: comparator doc

--- src/Ouzo/Utilities/Comparator.php
+++ src/Ouzo/Utilities/Comparator.php
@@ -6,19 +6,36 @@ class Comparator
 {
 
     /**
+     * Combines comparators into one resolving order using first comparator and resolving conflicts using tie breakers.
+     * Second provided comparator is first tie breaker, third is second tie breaker and so on.
+     *
      * @param mixed ...
-     * @return \Ouzo\Utilities\CompoundComparator
+     * @return comparator
      */
     public static function compound()
     {
         return new CompoundComparator(func_get_args());
     }
 
+    /**
+     * Returns comparator which compares objects by using values computed using given expression.
+     * Expression should comply with format accepted by <code>Functions::extractExpression</code>.
+     * Comparator returns an integer less than, equal to, or greater than zero if the first argument is considered to be respectively less than, equal to, or greater than the second.
+     *
+     * @param $expression
+     * @return comparator
+     */
     public static function compareBy($expression)
     {
         return new EvaluatingComparator(Functions::extractExpression($expression));
     }
 
+    /**
+     * Returns comparator according to which order between element is reversed.
+     *
+     * @param $comparator
+     * @return comparator
+     */
     public static function reverse($comparator)
     {
         return new ReversedComparator($comparator);
