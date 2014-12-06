fb81b0e0b900ae107f6e25823e0cd4d17dadffbe
piotrooo <piotroo89@gmail.com>
2014-12-06 11:49:56 +0100

    PHPDoc for Cache.

--- src/Ouzo/Utilities/Cache.php
+++ src/Ouzo/Utilities/Cache.php
@@ -3,18 +3,25 @@ namespace Ouzo\Utilities;
 
 use ReflectionFunction;
 
+/**
+ * Class Cache
+ * @package Ouzo\Utilities
+ */
 class Cache
 {
     private static $_cache = array();
 
     /**
      * Caches the given closure using filename:line as a key.
+     *
+     * Example:
      * <code>
      * $countries = Cache::memoize(function() {{
      *    //expensive computation that returns a list of countries
      *    return Country:all();
      * })
      * </code>
+     *
      * @param $function
      * @return mixed|null
      */
@@ -29,12 +36,14 @@ class Cache
      * Returns object from cache.
      * If there's no object for the given key and $functions is passed, $function result will be stored in cache under the given key.
      *
+     * Example:
      * <code>
      * $countries = Cache::get("countries", function() {{
      *    //expensive computation that returns a list of countries
      *    return Country:all();
      * })
      * </code>
+     *
      * @param $key
      * @param null $function
      * @return mixed|null
@@ -47,21 +56,40 @@ class Cache
         return Arrays::getValue(self::$_cache, $key);
     }
 
+    /**
+     * @param $key
+     * @param $object
+     * @return mixed
+     */
     public static function put($key, $object)
     {
         return self::$_cache[$key] = $object;
     }
 
+    /**
+     * Check is cache contains objects stored under key.
+     *
+     * @param $key
+     * @return bool
+     */
     public static function contains($key)
     {
         return isset(self::$_cache[$key]);
     }
 
+    /**
+     * Returns number of stored objects.
+     *
+     * @return int
+     */
     public static function size()
     {
         return count(self::$_cache);
     }
 
+    /**
+     * Clear all items stored in cache.
+     */
     public static function clear()
     {
         self::$_cache = array();
