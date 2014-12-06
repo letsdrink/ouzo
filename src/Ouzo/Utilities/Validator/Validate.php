72e1020a36e7ef78f3d5ca0a332c043dd78a54e8
piotrooo <piotroo89@gmail.com>
2014-12-06 14:26:40 +0100

    PHPDoc for Validate.

--- src/Ouzo/Utilities/Validator/Validate.php
+++ src/Ouzo/Utilities/Validator/Validate.php
@@ -1,8 +1,20 @@
 <?php
 namespace Ouzo\Utilities\Validator;
 
+/**
+ * Class Validate
+ * @package Ouzo\Utilities\Validator
+ */
 class Validate
 {
+    /**
+     * Check is value is true, otherwise throw ValidateException with the user given message.
+     *
+     * @param bool $value
+     * @param string $message
+     * @return bool
+     * @throws ValidateException
+     */
     public static function isTrue($value, $message = '')
     {
         if ($value !== true) {
@@ -11,6 +23,14 @@ class Validate
         return true;
     }
 
+    /**
+     * Check is value is correct email address, otherwise throw ValidateException with the user given message.
+     *
+     * @param string $value
+     * @param string $message
+     * @return bool
+     * @throws ValidateException
+     */
     public static function isEmail($value, $message = '')
     {
         if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
@@ -19,6 +39,14 @@ class Validate
         return true;
     }
 
+    /**
+     * Check is value is not null, otherwise throw ValidateException with the user given message.
+     *
+     * @param mixed $value
+     * @param string $message
+     * @return bool
+     * @throws ValidateException
+     */
     public static function isNotNull($value, $message = '')
     {
         if ($value === null) {
--- src/Ouzo/Utilities/Validator/ValidateException.php
+++ src/Ouzo/Utilities/Validator/ValidateException.php
@@ -3,6 +3,10 @@ namespace Ouzo\Utilities\Validator;
 
 use Exception;
 
+/**
+ * Class ValidateException
+ * @package Ouzo\Utilities\Validator
+ */
 class ValidateException extends Exception
 {
 }
