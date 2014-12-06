d0552bdf4b27733c4692e020466da0e978b15b96
piotrooo <piotroo89@gmail.com>
2014-12-06 12:21:21 +0100

    PHPDoc for StrSubstitutor.

--- src/Ouzo/Utilities/StrSubstitutor.php
+++ src/Ouzo/Utilities/StrSubstitutor.php
@@ -1,17 +1,54 @@
 <?php
 namespace Ouzo\Utilities;
 
+/**
+ * Class StrSubstitutor
+ * @package Ouzo\Utilities
+ */
 class StrSubstitutor
 {
     private $_values;
     private $_default;
 
+    /**
+     * Create substitutor object, and define placeholders - values map for the replacements.
+     * Substitutor can use default value for the replacements when not found value in the defined map.
+     *
+     * @param array $values
+     * @param null|string $default
+     */
     public function __construct($values = array(), $default = null)
     {
         $this->_values = $values;
         $this->_default = $default;
     }
 
+    /**
+     * Replaces placeholders {{PLACEHOLDER_NAME}} for the defined value.
+     *
+     * Example:
+     * <code>
+     * $strSubstitutor = new StrSubstitutor(array('NAME' => 'John', 'SURNAME' => 'Smith'));
+     * $substituted = $strSubstitutor->replace('Hi, {{NAME}} {{SURNAME}}');
+     * </code>
+     * Result:
+     * <code>
+     * Hi, John Smith
+     * </code>
+     *
+     * Example:
+     * <code>
+     * $strSubstitutor = new StrSubstitutor(array(), 'Unknown');
+     * $substituted = $strSubstitutor->replace('Hi, {{NAME}}');
+     * </code>
+     * Result:
+     * <code>
+     * Hi, Unknown
+     * </code>
+     *
+     * @param string $string
+     * @return mixed
+     */
     public function replace($string)
     {
         return preg_replace_callback('/\{\{(\w+)}}/', array($this, '_replace_vars'), $string);
