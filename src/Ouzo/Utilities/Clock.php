3c895bbbc7dd5ee14a33e3fe95d3ccd622423630
piotrooo <piotroo89@gmail.com>
2014-12-06 11:57:37 +0100

    PHPDoc for Clock.

--- src/Ouzo/Utilities/Clock.php
+++ src/Ouzo/Utilities/Clock.php
@@ -3,11 +3,20 @@ namespace Ouzo\Utilities;
 
 use DateTime;
 
+/**
+ * Class Clock
+ * @package Ouzo\Utilities
+ */
 class Clock
 {
     public static $freeze = false;
     public static $freezeDate;
 
+    /**
+     * Freeze clock on given date, if given null or none freeze on now.
+     *
+     * @param null $date
+     */
     public static function freeze($date = null)
     {
         self::$freeze = false;
@@ -15,11 +24,32 @@ class Clock
         self::$freeze = true;
     }
 
+    /**
+     * Return format date which is currently freeze.
+     *
+     * Example:
+     * <code>
+     * Clock::freeze('2011-01-02 12:34');
+     * $result = Clock::nowAsString('Y-m-d');
+     * </code>
+     * Result:
+     * <code>
+     * 2011-01-02
+     * </code>
+     *
+     * @param string $format
+     * @return string
+     */
     public static function nowAsString($format = 'Y-m-d H:i:s')
     {
         return self::now()->format($format);
     }
 
+    /**
+     * Return DateTime object which is currently freeze.
+     *
+     * @return DateTime
+     */
     public static function now()
     {
         $date = new DateTime();
