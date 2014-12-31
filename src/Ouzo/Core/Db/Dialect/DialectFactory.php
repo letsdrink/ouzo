b0c354b9d08ecebb67a7f22d534086a06cd4024c
piotrooo <piotroo89@gmail.com>
2014-12-31 20:51:20 +0100

    Validate dialect in DialectFactory (issue #154).

--- src/Ouzo/Core/Db/Dialect/DialectFactory.php
+++ src/Ouzo/Core/Db/Dialect/DialectFactory.php
@@ -1,16 +1,29 @@
 <?php
 namespace Ouzo\Db\Dialect;
 
+use Exception;
 use Ouzo\Config;
 
 class DialectFactory
 {
     /**
      * @return Dialect
+     * @throws Exception
      */
     public static function create()
     {
         $dialectClass = Config::getValue('sql_dialect');
+        self::_validateDialect($dialectClass);
         return new $dialectClass();
+    }
+
+    private static function _validateDialect($dialectClass)
+    {
+        if (!$dialectClass) {
+            throw new Exception('SQL dialect is not given. Please check config option - sql_dialect.');
+        }
+        if (!$dialectClass instanceof Dialect) {
+            throw new Exception('Invalid sql dialect. Dialect should extends from Dialect class.');
+        }
     }
 }
