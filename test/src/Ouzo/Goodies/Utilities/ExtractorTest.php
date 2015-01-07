d0bd0da70796d4d200d63f953ed2df747b094582
woru <wor18@wp.pl>
2015-01-07 11:05:07 +0100

    array access for empty extractor

--- test/src/Ouzo/Goodies/Utilities/ExtractorTest.php
+++ test/src/Ouzo/Goodies/Utilities/ExtractorTest.php
@@ -173,13 +173,31 @@ class ExtractorTest extends DbTransactionalTestCase
     /**
      * @test
      */
-    public function shouldExtractArrayValue()
+    public function shouldExtractArrayValueFromField()
     {
         //given
         $object = new stdClass();
         $object->field1 = array('key' => 'value');
 
         $function = Functions::extract()->field1['key'];
+
+        //when
+        $result = Functions::call($function, $object);
+
+        //then
+        Assert::thatString($result)->isEqualTo('value');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldExtractArrayValue()
+    {
+        //given
+        $object = array('key' => 'value');
+
+        //$function = Functions::extract()['key']; only i php 5.4
+        $function = Functions::extract()->offsetGet('key');
 
         //when
         $result = Functions::call($function, $object);
