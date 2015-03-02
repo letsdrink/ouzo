6b9f1c666e0eebb16b59afb07fea9ec7a2c4462c
Bartosz Bańkowski <bbankowski@gmail.com>
2015-03-02 17:05:29 +0100

    Implemented memoizing supplier.

--- test/src/Ouzo/Goodies/Utilities/SuppliersTest.php
+++ test/src/Ouzo/Goodies/Utilities/SuppliersTest.php
@@ -13,7 +13,7 @@ class SuppliersTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function shouldCacheResultForGivenTime()
+    public function memoizeWithExpirationShouldCacheResultForGivenTime()
     {
         //given
         $command = Mock::mock();
@@ -39,6 +39,37 @@ class SuppliersTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('Jack', $result3);
         $this->assertEquals('Black', $result4);
         $this->assertEquals('Black', $result5);
+    }
+
+    /**
+     * @test
+     */
+    public function memoizeShouldCacheResult()
+    {
+        //given
+        $command = Mock::mock();
+        Mock::when($command)->getName()->thenReturn('Jack', 'Black', 'White');
+
+        $supplier = Suppliers::memoize(function () use ($command) {
+            return $command->getName();
+        });
+
+        //when
+        Clock::freeze('2014-01-01 11:11:11');
+        $result1 = $supplier->get();
+        $result2 = $supplier->get();
+        Clock::freeze('2014-01-01 11:11:12');
+        $result3 = $supplier->get();
+        Clock::freeze('2014-01-01 11:11:22');
+        $result4 = $supplier->get();
+        $result5 = $supplier->get();
+
+        //then
+        $this->assertEquals('Jack', $result1);
+        $this->assertEquals('Jack', $result2);
+        $this->assertEquals('Jack', $result3);
+        $this->assertEquals('Jack', $result4);
+        $this->assertEquals('Jack', $result5);
     }
 
     protected function tearDown()
