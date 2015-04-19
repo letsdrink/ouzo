197c606df1900403cf4cc0aecf6295422bd718de
bbankowski <bbankowski@gmail.com>
2015-04-19 22:47:09 +0200

    Added test cases for phtml renderer (issue #37).

---
+++ test/src/Ouzo/Core/View/PhtmlRendererTest.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Copyright (c) Ouzo contributors, http://ouzoframework.org
+ * This file is made available under the MIT License (view the LICENSE file for more information).
+ */
+
+use Ouzo\Config;
+use Ouzo\View\PhtmlRenderer;
+
+class PhtmlRendererTest extends PHPUnit_Framework_TestCase {
+
+    public function setUp()
+    {
+        parent::setUp();
+        Config::overrideProperty('path', 'view')->with('test\src\Ouzo\Core\View');
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        Config::revertProperty('path', 'view');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldRenderView()
+    {
+        //given
+        $renderer = new PhtmlRenderer('hello_world', array());
+
+        //when
+        $result = $renderer->render();
+
+        //then
+        $this->assertEquals('Hello World!', $result);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldRenderViewWithAttributes()
+    {
+        //given
+        $renderer = new PhtmlRenderer('hello', array('name' => 'Jack'));
+
+        //when
+        $result = $renderer->render();
+
+        //then
+        $this->assertEquals('Hello Jack!', $result);
+    }
+}
+ 
