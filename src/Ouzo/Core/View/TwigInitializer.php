68440c2495f03592e55564aafbcb59617c641851
Bartosz Bańkowski <bbankowski@gmail.com>
2015-04-22 14:58:27 +0200

    Added twig initializer.

--- src/Ouzo/Core/View/TwigInitializer.php
+++ src/Ouzo/Core/View/TwigInitializer.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * Copyright (c) Ouzo contributors, http://ouzoframework.org
+ * This file is made available under the MIT License (view the LICENSE file for more information).
+ */
 namespace Ouzo\View;
 
 use Twig_Environment;
