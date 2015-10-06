5d8d429c27e2c0ab92faf8616fbdc0280d2a610d
Bartosz Bańkowski <bbankowski@gmail.com>
2015-10-06 16:15:40 +0200

    Added missing copyright notes.

--- test/src/Ouzo/Core/ControllerParametersTest.php
+++ test/src/Ouzo/Core/ControllerParametersTest.php
@@ -1,6 +1,8 @@
 <?php
-
-
+/*
+ * Copyright (c) Ouzo contributors, http://ouzoframework.org
+ * This file is made available under the MIT License (view the LICENSE file for more information).
+ */
 use Ouzo\ControllerParameters;
 
 class ControllerParametersTest extends PHPUnit_Framework_TestCase
