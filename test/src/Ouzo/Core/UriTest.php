e6f790251d0d95696dd308ebf4e7763d4ca61c4a
piotrooo <piotroo89@gmail.com>
2016-10-03 08:39:39 +0200

    Fixed mock issues.

--- test/src/Ouzo/Core/UriTest.php
+++ test/src/Ouzo/Core/UriTest.php
@@ -7,6 +7,7 @@ use Ouzo\Config;
 use Ouzo\ContentType;
 use Ouzo\Tests\ArrayAssert;
 use Ouzo\Tests\CatchException;
+use Ouzo\Tests\Mock\Mock;
 use Ouzo\Tests\StreamStub;
 use Ouzo\Uri;
 
@@ -15,13 +16,13 @@ class UriTest extends PHPUnit_Framework_TestCase
     /**
      * @var Uri
      */
-    private $_uri;
-    private $_pathProviderMock;
+    private $uri;
+    private $pathProviderMock;
 
     public function setUp()
     {
-        $this->_pathProviderMock = $this->getMock('\Ouzo\Uri\PathProvider', array('getPath'));
-        $this->_uri = new Uri($this->_pathProviderMock);
+        $this->pathProviderMock = Mock::create('\Ouzo\Uri\PathProvider');
+        $this->uri = new Uri($this->pathProviderMock);
     }
 
     /**
@@ -33,8 +34,8 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(Config::getPrefixSystem() . '/user/add/id/5/name/john');
 
         //then
-        $this->assertEquals('User', $this->_uri->getController());
-        $this->assertEquals('user', $this->_uri->getRawController());
+        $this->assertEquals('User', $this->uri->getController());
+        $this->assertEquals('user', $this->uri->getRawController());
     }
 
     /**
@@ -46,7 +47,7 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(Config::getPrefixSystem() . '/user/add/id/5/name/john');
 
         //then
-        $this->assertEquals('add', $this->_uri->getAction());
+        $this->assertEquals('add', $this->uri->getAction());
     }
 
     /**
@@ -58,8 +59,8 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(Config::getPrefixSystem() . '/user/add/id/5/name/john');
 
         //then
-        $this->assertEquals('john', $this->_uri->getParam('name'));
-        $this->assertEquals(5, $this->_uri->getParam('id'));
+        $this->assertEquals('john', $this->uri->getParam('name'));
+        $this->assertEquals(5, $this->uri->getParam('id'));
     }
 
     /**
@@ -71,7 +72,7 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(Config::getPrefixSystem() . '/user/add/id/5');
 
         //then
-        $this->assertNull($this->_uri->getParam('surname'));
+        $this->assertNull($this->uri->getParam('surname'));
     }
 
     /**
@@ -83,7 +84,7 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(Config::getPrefixSystem() . '/user/add');
 
         //then
-        $this->assertNull($this->_uri->getParam('surname'));
+        $this->assertNull($this->uri->getParam('surname'));
     }
 
     /**
@@ -95,7 +96,7 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(Config::getPrefixSystem() . '/user/add/id/5/name');
 
         //when
-        $param = $this->_uri->getParam('name');
+        $param = $this->uri->getParam('name');
 
         //then
         $this->assertNull($param);
@@ -142,7 +143,7 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(Config::getPrefixSystem() . '/user/add/id/5/name/john/surname/smith/');
 
         //when
-        $params = $this->_uri->getParams();
+        $params = $this->uri->getParams();
         $paramsExpected = array('id' => 5, 'name' => 'john', 'surname' => 'smith');
 
         //then
@@ -173,7 +174,7 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(Config::getPrefixSystem() . '/user/add/id/5?param1=val1&param2=val2');
 
         //when
-        $params = $this->_uri->getParams();
+        $params = $this->uri->getParams();
         $paramsExpected = array('id' => 5, 'param1' => 'val1', 'param2' => 'val2');
 
         //then
@@ -189,7 +190,7 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(Config::getPrefixSystem() . '/user/add?param1=val1&param2=val2&param3=t1%2Ct2%2Ct3');
 
         //when
-        $params = $this->_uri->getParams();
+        $params = $this->uri->getParams();
         $paramsExpected = array('param1' => 'val1', 'param2' => 'val2', 'param3' => 't1,t2,t3');
 
         //then
@@ -205,7 +206,7 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(Config::getPrefixSystem() . '/user/add/id/4?param1=path/to/file&param2=val2');
 
         //when
-        $params = $this->_uri->getParams();
+        $params = $this->uri->getParams();
         $paramsExpected = array('id' => 4, 'param1' => 'path/to/file', 'param2' => 'val2');
 
         //then
@@ -337,7 +338,7 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(Config::getPrefixSystem() . $broken);
 
         //when
-        $path = $this->_uri->getPathWithoutPrefix();
+        $path = $this->uri->getPathWithoutPrefix();
 
         //then
         $this->assertEquals($good, $path);
@@ -352,7 +353,7 @@ class UriTest extends PHPUnit_Framework_TestCase
         $this->_path(null);
 
         //when
-        $path = $this->_uri->getAction();
+        $path = $this->uri->getAction();
 
         //then
         $this->assertNull($path);
@@ -398,9 +399,7 @@ class UriTest extends PHPUnit_Framework_TestCase
 
     private function _path($path)
     {
-        $this->_pathProviderMock->expects($this->any())
-            ->method('getPath')
-            ->will($this->returnValue($path));
+        Mock::when($this->pathProviderMock)->getPath()->thenReturn($path);
     }
 
     private function _privateMethod($testMethod)
