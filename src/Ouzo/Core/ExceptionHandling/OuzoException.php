278edd8f162c760332aac144a9718c035aa48ea6
andrzejo <andrzej.oczkowicz@gmail.com>
2017-03-24 16:37:10 +0100

    Add getErrorMessages to OuzoException

--- src/Ouzo/Core/ExceptionHandling/OuzoException.php
+++ src/Ouzo/Core/ExceptionHandling/OuzoException.php
@@ -3,10 +3,12 @@
  * Copyright (c) Ouzo contributors, http://ouzoframework.org
  * This file is made available under the MIT License (view the LICENSE file for more information).
  */
+
 namespace Ouzo\ExceptionHandling;
 
 use Exception;
 use Ouzo\Utilities\Arrays;
+use Ouzo\Utilities\Functions;
 
 class OuzoException extends Exception
 {
@@ -14,6 +16,12 @@ class OuzoException extends Exception
     private $_errors;
     private $_headers = array();
 
+    /**
+     * OuzoException constructor.
+     * @param int $httpCode
+     * @param Error[]|\Error $errors
+     * @param string[] $headers
+     */
     public function __construct($httpCode, $errors, $headers = array())
     {
         $this->_httpCode = $httpCode;
@@ -32,6 +40,11 @@ class OuzoException extends Exception
     public function getErrors()
     {
         return $this->_errors;
+    }
+
+    public function getErrorMessages()
+    {
+        return Arrays::map($this->_errors, Functions::extractField('message'));
     }
 
     public function asExceptionData()
---
+++ src/test/Ouzo/Core/ExceptionHandling/OuzoExceptionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ouzo\ExceptionHandling;
+
+use Ouzo\Tests\Assert;
+
+class OuzoExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldReturnErrorMessages()
+    {
+        //given
+        $ouzoException = new OuzoException(500, [new Error(0, "Error 1"), new Error(1, "Error 2")]);
+
+        //when
+        $messages = $ouzoException->getErrorMessages();
+
+        //then
+        Assert::thatArray($messages)
+            ->containsOnly("Error 1", "Error 2");
+    }
+}
