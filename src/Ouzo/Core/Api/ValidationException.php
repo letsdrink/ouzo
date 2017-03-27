31121d2f85cb8f7b4138e9706d6bf061e3174915
andrzejo <andrzej.oczkowicz@gmail.com>
2017-03-27 10:00:33 +0200

    Add constructor param types to ValidationException

--- src/Ouzo/Core/Api/ValidationException.php
+++ src/Ouzo/Core/Api/ValidationException.php
@@ -3,14 +3,21 @@
  * Copyright (c) Ouzo contributors, http://ouzoframework.org
  * This file is made available under the MIT License (view the LICENSE file for more information).
  */
+
 namespace Ouzo\Api;
 
+use Ouzo\ExceptionHandling\Error;
 use Ouzo\ExceptionHandling\OuzoException;
 
 class ValidationException extends OuzoException
 {
     const HTTP_CODE = 400;
 
+    /**
+     * @param int $httpCode
+     * @param Error|Error[]| $errors
+     * @param string[] $headers
+     */
     public function __construct($errors, $headers = array())
     {
         parent::__construct(self::HTTP_CODE, $errors, $headers);
