543bb8c8aed948395e406c2d001d75cc6d59144c
dpawlik <dominik.pawlik@thulium.pl>
2017-11-21 10:46:05 +0100

    Added quick docs to Validatable class

--- src/Ouzo/Core/Validatable.php
+++ src/Ouzo/Core/Validatable.php
@@ -3,6 +3,7 @@
  * Copyright (c) Ouzo contributors, http://ouzoframework.org
  * This file is made available under the MIT License (view the LICENSE file for more information).
  */
+
 namespace Ouzo;
 
 use Ouzo\ExceptionHandling\Error;
@@ -22,6 +23,9 @@ class Validatable
         return empty($errors);
     }
 
+    /**
+     * @return array - returns array with saved errors
+     */
     public function getErrors()
     {
         return Arrays::map($this->_errors, Functions::extractField('message'));
@@ -60,6 +64,12 @@ class Validatable
         }
     }
 
+    /**
+     * Check whether passed string in $value parameter has 0 length or not
+     * @param string $value - string to check
+     * @param $errorMessage - error message
+     * @param null $errorField
+     */
     public function validateNotBlank($value, $errorMessage, $errorField = null)
     {
         if (Strings::isBlank($value)) {
@@ -68,6 +78,13 @@ class Validatable
         }
     }
 
+    /**
+     * Checks whether value is true, if not it saves error
+     * @param $value - value to check whether is true
+     * (values which are considered as TRUE or FALSE are presented here http://php.net/manual/en/types.comparisons.php )
+     * @param $errorMessage - error message
+     * @param null $errorField
+     */
     public function validateTrue($value, $errorMessage, $errorField = null)
     {
         if (!$value) {
@@ -76,6 +93,12 @@ class Validatable
         }
     }
 
+    /**
+     * Checks whether array does not contain duplicate values
+     * @param array $values - array to check
+     * @param $errorMessage - error message
+     * @param null $errorField
+     */
     public function validateUnique(array $values, $errorMessage, $errorField = null)
     {
         if (count($values) != count(array_unique($values))) {
@@ -84,6 +107,12 @@ class Validatable
         }
     }
 
+    /**
+     * Checks whether $value can be converted to time by "strtotime" function
+     * @param string $value - string to check
+     * @param $errorMessage - error message
+     * @param null $errorField
+     */
     public function validateDateTime($value, $errorMessage, $errorField = null)
     {
         if (!strtotime($value)) {
@@ -92,6 +121,13 @@ class Validatable
         }
     }
 
+    /**
+     * Checks whether string does not exceed max length
+     * @param string $value - string to check
+     * @param int $maxLength - max length which doesn't cause error
+     * @param $errorMessage - error message
+     * @param null $errorField
+     */
     public function validateStringMaxLength($value, $maxLength, $errorMessage, $errorField = null)
     {
         if (strlen($value) > $maxLength) {
@@ -100,6 +136,13 @@ class Validatable
         }
     }
 
+    /**
+     * Checks whether $value is not empty
+     * (table which explains that is here http://php.net/manual/en/types.comparisons.php)
+     * @param $value - value to check
+     * @param $errorMessage - error message
+     * @param null $errorField
+     */
     public function validateNotEmpty($value, $errorMessage, $errorField = null)
     {
         if (empty($value)) {
@@ -108,6 +151,13 @@ class Validatable
         }
     }
 
+    /**
+     * Validate whether $value is empty
+     * (table which explains that is here http://php.net/manual/en/types.comparisons.php)
+     * @param $value - value to check
+     * @param $errorMessage - error message
+     * @param null $errorField
+     */
     public function validateEmpty($value, $errorMessage, $errorField = null)
     {
         if (!empty($value)) {
@@ -116,11 +166,21 @@ class Validatable
         }
     }
 
+    /**
+     * Method for adding error manually
+     * @param string|\Ouzo\ExceptionHandling\Error $error - \Ouzo\ExceptionHandling\Error instance or new error message
+     * @param int $code - error code
+     */
     public function error($error, $code = 0)
     {
         $this->_errors[] = $error instanceof Error ? $error : new Error($code, $error);
     }
 
+    /**
+     * Method for batch adding errors manually
+     * @param array $errors - array of \Ouzo\ExceptionHandling\Error instaces or new error messages
+     * @param int $code
+     */
     public function errors(array $errors, $code = 0)
     {
         foreach ($errors as $error) {
