40dd4467aa6b2a145206d2d42d497756f89ea57b
Bartosz Bańkowski <bbankowski@gmail.com>
2017-12-06 16:11:58 +0100

    Store complete data object inside mock.

--- src/Ouzo/Core/Tests/MockDownloadHandler.php
+++ src/Ouzo/Core/Tests/MockDownloadHandler.php
@@ -3,26 +3,34 @@
  * Copyright (c) Ouzo contributors, http://ouzoframework.org
  * This file is made available under the MIT License (view the LICENSE file for more information).
  */
+
 namespace Ouzo\Tests;
+
+use Ouzo\Utilities\Arrays;
 
 class MockDownloadHandler
 {
-    private $_fileName;
+    private $fileData;
 
     public function downloadFile($fileData)
     {
-        $this->_fileName = $fileData['label'];
+        $this->fileData = $fileData;
         return $this;
     }
 
     public function streamMediaFile(array $fileData)
     {
-        $this->_fileName = $fileData['label'];
+        $this->fileData = $fileData;
         return $this;
     }
 
     public function getFileName()
     {
-        return $this->_fileName;
+        return Arrays::getValue($this->fileData, 'label');
+    }
+
+    public function getFileData()
+    {
+        return Arrays::getValue($this->fileData, 'data');
     }
 }
