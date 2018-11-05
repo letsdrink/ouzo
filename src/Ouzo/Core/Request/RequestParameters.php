abd1ab97c958dce1d2be1d20d394dc3e6ae285d8
polaszewski <piotroo89@gmail.com>
2018-11-05 14:32:12 +0100

    Elvis.

--- src/Ouzo/Core/Request/RequestParameters.php
+++ src/Ouzo/Core/Request/RequestParameters.php
@@ -23,7 +23,7 @@ class RequestParameters
 
     public function get($stream = 'php://input')
     {
-        $parameters = $this->routingService->getRouteRule()->getParameters() ? $this->routingService->getRouteRule()->getParameters() : $this->routingService->getUri()->getParams();
+        $parameters = $this->routingService->getRouteRule()->getParameters() ?: $this->routingService->getUri()->getParams();
         $requestParameters = Uri::getRequestParameters($stream);
         return array_merge($parameters, $_POST, $_GET, $requestParameters);
     }
