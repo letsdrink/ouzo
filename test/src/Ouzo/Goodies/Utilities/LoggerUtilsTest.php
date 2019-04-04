d4b6a514fbb85cfef03057571dd2521e3492199d
polaszewski <piotroo89@gmail.com>
2019-04-04 15:06:39 +0200

    Stortener classes to logs.

---
+++ test/src/Ouzo/Goodies/Utilities/LoggerUtilsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ouzo\Utilities;
+
+use PHPUnit\Framework\TestCase;
+
+class LoggerUtilsTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider classes
+     */
+    public function shouldShortenName($class, $length, $expected)
+    {
+        //when
+        $shortenClassName = LoggerUtils::shortenClassName($class, $length);
+
+        //then
+        $this->assertEquals($expected, $shortenClassName);
+    }
+
+    public function classes()
+    {
+        return [
+            ['Ouzo\Utilities\LoggerUtils', null, 'Ouzo\Utilities\LoggerUtils'],
+            ['Ouzo\Utilities\LoggerUtils', 0, 'LoggerUtils'],
+            ['Ouzo\Utilities\LoggerUtils', 10, 'O\U\LoggerUtils'],
+            ['Really\Long\Name\Space\To\The\Class', 1, 'R\L\N\S\T\T\Class'],
+            ['Really\Long\Name\Space\To\The\Class', 10, 'R\L\N\S\T\T\Class'],
+            ['Really\Long\Name\Space\To\The\Class', 20, 'R\L\N\S\To\The\Class'],
+            ['The\Class', 9, 'The\Class'],
+            ['Class', 1, 'Class'],
+            ['Class', 10, 'Class'],
+            ['My Wrong Class', 10, 'My Wrong Class'],
+        ];
+    }
+}
