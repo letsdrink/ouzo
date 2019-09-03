e1d3d0109a1dcad623d20914f16c98bcdb180cb7
Bartosz Bańkowski <bbankowski@gmail.com>
2019-09-03 15:12:52 +0200

    Added more verbose logging of request.

--- src/Ouzo/Core/Middleware/Interceptor/LogRequest.php
+++ src/Ouzo/Core/Middleware/Interceptor/LogRequest.php
@@ -8,11 +8,24 @@ namespace Ouzo\Middleware\Interceptor;
 
 use Ouzo\Logger\Logger;
 use Ouzo\Request\RequestContext;
+use Ouzo\Uri;
+use Ouzo\Uri\PathProvider;
+use Ouzo\Uri\PathProviderInterface;
 use Ouzo\Utilities\Chain\Chain;
 use Ouzo\Utilities\Chain\Interceptor;
 
 class LogRequest implements Interceptor
 {
+    private $pathProvider;
+
+    /**
+     * @Inject
+     */
+    public function __construct(PathProviderInterface $pathProvider)
+    {
+        $this->pathProvider = $pathProvider;
+    }
+
     /**
      * @param RequestContext $requestContext
      * @param Chain $next
@@ -20,8 +33,12 @@ class LogRequest implements Interceptor
      */
     public function handle($requestContext, Chain $next)
     {
-        Logger::getLogger(__CLASS__)
-            ->info('[Request:/%s/%s]', [$requestContext->getCurrentController(), $requestContext->getCurrentAction()]);
+        Logger::getLogger(__CLASS__)->info('[Action: %s#%s] [Request: %s %s]', [
+            $requestContext->getCurrentController(),
+            $requestContext->getCurrentAction(),
+            Uri::getRequestType(),
+            $this->pathProvider->getPath()
+        ]);
 
         return $next->proceed($requestContext);
     }
