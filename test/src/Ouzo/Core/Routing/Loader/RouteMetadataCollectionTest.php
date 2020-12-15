bedbd937eea793b4752adbe5c82f1021a4704ce3
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-12-15 21:57:54 +0100

    Add missing Ouzo copyright notice

--- test/src/Ouzo/Core/Routing/Loader/RouteMetadataCollectionTest.php
+++ test/src/Ouzo/Core/Routing/Loader/RouteMetadataCollectionTest.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * Copyright (c) Ouzo contributors, http://ouzoframework.org
+ * This file is made available under the MIT License (view the LICENSE file for more information).
+ */
 
 use Ouzo\Routing\Loader\RouteMetadata;
 use Ouzo\Routing\Loader\RouteMetadataCollection;
