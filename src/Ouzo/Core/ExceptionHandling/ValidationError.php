e0d8bcc3a7a42420b99d04eb6a4491cf418b2ffd
Bartosz Bańkowski <bartosz.bankowski@thulium.pl>
2020-12-16 16:11:00 +0100

    [MVC] Fixed request parameter validation.

--- src/Ouzo/Core/ExceptionHandling/ValidationError.php
+++ src/Ouzo/Core/ExceptionHandling/ValidationError.php
@@ -8,10 +8,13 @@ namespace Ouzo\ExceptionHandling;
 
 class ValidationError extends Error
 {
+    private ?string $value;
 
-    public function __construct(string $message, ?string $field = null)
+    public function __construct(string $message, ?string $field = null, ?string $value = null, ?string $code = null)
     {
         parent::__construct(null, $message, null, $field);
+        $this->value = $value;
+        $this->code = $code;
     }
 
     public function toArray()
@@ -19,6 +22,12 @@ class ValidationError extends Error
         $array = ['message' => $this->getMessage()];
         if ($this->getField()) {
             $array['field'] = $this->getField();
+        }
+        if ($this->getCode()) {
+            $array['code'] = $this->code;
+        }
+        if ($this->value) {
+            $array['value'] = $this->value;
         }
         return $array;
     }
