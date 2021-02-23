4270685246f557aef683b4d51c84e7c8871c0314
Bartosz Bańkowski <bartosz.bankowski@thulium.pl>
2021-02-23 13:01:45 +0100

    Fixed php 8.0 incompatibility.

--- src/Ouzo/Core/Db/PDOPreparedStatementExecutor.php
+++ src/Ouzo/Core/Db/PDOPreparedStatementExecutor.php
@@ -3,6 +3,7 @@
  * Copyright (c) Ouzo contributors, http://ouzoframework.org
  * This file is made available under the MIT License (view the LICENSE file for more information).
  */
+
 namespace Ouzo\Db;
 
 use PDOException;
@@ -14,18 +15,18 @@ class PDOPreparedStatementExecutor extends PDOExecutor
      */
     public function createPDOStatement($dbHandle, $sql, $boundValues, $queryString, $options = [])
     {
-        $pdoStatement = $dbHandle->prepare($sql, $options);
-
-        if (!$pdoStatement) {
-            throw PDOExceptionExtractor::getException($dbHandle->errorInfo(), $queryString);
-        }
-
-        foreach ($boundValues as $key => $valueBind) {
-            $type = ParameterType::getType($valueBind);
-            $pdoStatement->bindValue($key + 1, $valueBind, $type);
-        }
-
         try {
+            $pdoStatement = $dbHandle->prepare($sql, $options);
+
+            if (!$pdoStatement) {
+                throw PDOExceptionExtractor::getException($dbHandle->errorInfo(), $queryString);
+            }
+
+            foreach ($boundValues as $key => $valueBind) {
+                $type = ParameterType::getType($valueBind);
+                $pdoStatement->bindValue($key + 1, $valueBind, $type);
+            }
+
             if (!$pdoStatement->execute()) {
                 throw PDOExceptionExtractor::getException($pdoStatement->errorInfo(), $queryString);
             }
