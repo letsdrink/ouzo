aa79c6eb13f1e1e5eb61696d0fa1a18a95675871
krzysztofpiorkowski <krzysztofpiotrpiorkowski@gmail.com>
2021-11-10 15:43:15 +0100

    Added Options route

---
+++ src/Ouzo/Core/Routing/Annotation/Route/Options.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Copyright (c) Ouzo contributors, https://github.com/letsdrink/ouzo
+ * This file is made available under the MIT License (view the LICENSE file for more information).
+ */
+
+namespace Ouzo\Routing\Annotation\Route;
+
+use Attribute;
+use Ouzo\Http\HttpMethod;
+use Ouzo\Routing\Annotation\Route;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+class Options extends Route
+{
+    public function __construct(string $path, ?int $httpResponseCode = null)
+    {
+        parent::__construct($path, [HttpMethod::OPTIONS], $httpResponseCode);
+    }
+}
