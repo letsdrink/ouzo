d463be928046f82b73d97c816b14bf4bc0fd7017
Piotr Olaszewski <piotr.olaszewski@thulium.pl>
2022-06-23 12:19:32 +0200

    Add dynamic route path from config

---
+++ test/src/Ouzo/Core/Routing/Annotation/RouteTest.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Copyright (c) Ouzo contributors, https://github.com/letsdrink/ouzo
+ * This file is made available under the MIT License (view the LICENSE file for more information).
+ */
+
+use Ouzo\Config;
+use Ouzo\Http\HttpMethod;
+use Ouzo\Routing\Annotation\Route;
+use PHPUnit\Framework\TestCase;
+
+class RouteTest extends TestCase
+{
+    public function tearDown(): void
+    {
+        Config::revertProperty('sample', 'route', 'path');
+
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReadPathFromConfig(): void
+    {
+        //given
+        Config::overrideProperty('sample', 'route', 'path')->with('/api/sample/path');
+
+        $route = new Route('${sample.route.path}', [HttpMethod::DELETE], null);
+
+        //when
+        $path = $route->getPath();
+
+        //then
+        $this->assertEquals('/api/sample/path', $path);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnConfigKeyWhenConfigValueIsNotSet(): void
+    {
+        //given
+        $route = new Route('${sample.route.path}', [HttpMethod::DELETE], null);
+
+        //when
+        $path = $route->getPath();
+
+        //then
+        $this->assertEquals('${sample.route.path}', $path);
+    }
+}
