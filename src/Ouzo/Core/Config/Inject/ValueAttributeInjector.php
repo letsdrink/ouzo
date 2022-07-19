499e3709731555ae9338bbae2eca325f01541e34
Bartosz Bańkowski <bartosz.bankowski@thulium.pl>
2022-07-19 15:45:29 +0200

    [Inject] Handle int default value properly. Added error handling for bool and int.

--- src/Ouzo/Core/Config/Inject/ValueAttributeInjector.php
+++ src/Ouzo/Core/Config/Inject/ValueAttributeInjector.php
@@ -8,9 +8,8 @@ namespace Ouzo\Config\Inject;
 
 use Ouzo\Config\ConfigValueSelector;
 use Ouzo\Injection\Annotation\AttributeInjector;
+use Ouzo\Injection\InjectorException;
 use Ouzo\Injection\InstanceFactory;
-use Ouzo\Utilities\Booleans;
-use Ouzo\Utilities\Strings;
 use ReflectionAttribute;
 use ReflectionMethod;
 use ReflectionNamedType;
@@ -23,7 +22,7 @@ class ValueAttributeInjector implements AttributeInjector
         foreach ($reflectionProperties as $reflectionProperty) {
             $attributes = $reflectionProperty->getAttributes(Value::class);
             if (!empty($attributes)) {
-                $configValue = $this->getConfigValue($attributes[0], $reflectionProperty->getType());
+                $configValue = $this->getConfigValue($attributes[0], $reflectionProperty->getName(), $reflectionProperty->getType(), $instance::class);
 
                 $reflectionProperty->setAccessible(true);
                 $reflectionProperty->setValue($instance, $configValue);
@@ -39,22 +38,50 @@ class ValueAttributeInjector implements AttributeInjector
             $attributes = $parameter->getAttributes(Value::class);
             if (!empty($attributes)) {
                 $parameterName = $parameter->getName();
-                $constructorParameters[$parameterName] = $this->getConfigValue($attributes[0], $parameter->getType());
+                $constructorParameters[$parameterName] = $this->getConfigValue($attributes[0], $parameter->getName(), $parameter->getType(), $constructor->class);
             }
         }
         return $constructorParameters;
     }
 
-    private function getConfigValue(ReflectionAttribute $attribute, ?ReflectionType $type): mixed
+    private function getConfigValue(ReflectionAttribute $attribute, string $parameterName, ?ReflectionType $type, string $class): mixed
     {
         /** @var Value $value */
         $value = $attribute->newInstance();
         $selector = $value->getSelector();
 
         $configValue = ConfigValueSelector::selectConfigValue($selector);
-        if (is_string($configValue) && Strings::isNotBlank($configValue) && $type instanceof ReflectionNamedType && $type->getName() === 'bool') {
-            return Booleans::toBoolean($configValue);
+        if (is_string($configValue) && $type instanceof ReflectionNamedType) {
+            return match ($type->getName()) {
+                'bool' => $this->handleBoolValue($configValue, $parameterName, $class, $selector),
+                'int' => $this->handleIntValue($configValue, $parameterName, $class, $selector),
+                default => $configValue
+            };
         }
         return $configValue;
+    }
+
+    private function handleBoolValue(string $value, string $parameterName, string $class, string $selector): bool
+    {
+        if ($value === 'true') {
+            return true;
+        }
+        if ($value === 'false') {
+            return false;
+        }
+        throw $this->getInjectorException('bool', $parameterName, $class, $value, $selector);
+    }
+
+    private function handleIntValue(string $value, string $parameterName, string $class, string $selector): int
+    {
+        if (ctype_digit($value)) {
+            return intval($value);
+        }
+        throw $this->getInjectorException('int', $parameterName, $class, $value, $selector);
+    }
+
+    private function getInjectorException(string $type, string $parameterName, string $class, string $value, string $selector): InjectorException
+    {
+        return new InjectorException("Cannot inject #[Value] to `{$parameterName}` for class `$class`. Invalid {$type} value: `{$value}` for selector: `{$selector}`");
     }
 }
