f0130a8bd2397410a3943fddade20d0830bc936c
Bartosz Bańkowski <bartosz.bankowski@thulium.pl>
2023-12-11 17:10:49 +0100

    Refactored exception/error handlers.

--- src/Ouzo/Core/ExceptionHandling/DebugErrorHandler.php
+++ src/Ouzo/Core/ExceptionHandling/DebugErrorHandler.php
@@ -11,27 +11,22 @@ use Whoops\Run;
 
 class DebugErrorHandler extends ErrorHandler
 {
-    protected static function getRun(): Run
+    public function handleError(int $errorNumber, string $errorString, string $errorFile, int $errorLine): void
+    {
+        $this->createWhoops()->handleError($errorNumber, $errorString, $errorFile, $errorLine);
+    }
+
+    public function handleShutdown(): void
+    {
+        $this->createWhoops()->handleShutdown();
+    }
+
+    private function createWhoops(): Run
     {
         error_reporting(E_ALL);
         $run = new Run();
         $run->pushHandler(new PrettyPageHandler());
         $run->pushHandler(new DebugErrorLogHandler());
         return $run;
-    }
-
-    protected static function getExceptionHandler(): ExceptionHandler
-    {
-        return new DebugExceptionHandler();
-    }
-
-    public static function handleError(int $errorNumber, string $errorString, string $errorFile, int $errorLine): void
-    {
-        self::getRun()->handleError($errorNumber, $errorString, $errorFile, $errorLine);
-    }
-
-    public static function handleShutdown(): void
-    {
-        self::getRun()->handleShutdown();
     }
 }
