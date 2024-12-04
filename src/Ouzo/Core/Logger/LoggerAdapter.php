7af00b30984f5a3c6861e407cacfe817bfa9f1fb
Krzysztof Sucherek <krzysztof.sucherek@thulium.pl>
2024-12-04 16:36:30 +0100

    Add implementing Psr\Log\LoggerInterface to LoggerAdapter.

--- src/Ouzo/Core/Logger/LoggerAdapter.php
+++ src/Ouzo/Core/Logger/LoggerAdapter.php
@@ -12,7 +12,7 @@ use Ouzo\Utilities\Strings;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 
-class LoggerAdapter
+class LoggerAdapter implements LoggerInterface
 {
     private MessageFormatter $messageFormatter;
     private ?ContextEnhancer $contextEnhancer;
@@ -33,49 +33,49 @@ class LoggerAdapter
         $this->minimalLevels = Arrays::getValue($loggerConfiguration, 'minimal_levels', []);
     }
 
-    public function emergency(string $message, array $context = []): void
+    public function emergency($message, array $context = []): void
     {
         $this->log(LogLevel::EMERGENCY, $message, $context);
     }
 
-    public function alert(string $message, array $context = []): void
+    public function alert($message, array $context = []): void
     {
         $this->log(LogLevel::ALERT, $message, $context);
     }
 
-    public function critical(string $message, array $context = []): void
+    public function critical($message, array $context = []): void
     {
         $this->log(LogLevel::CRITICAL, $message, $context);
     }
 
-    public function error(string $message, array $context = []): void
+    public function error($message, array $context = []): void
     {
         $this->log(LogLevel::ERROR, $message, $context);
     }
 
-    public function warning(string $message, array $context = []): void
+    public function warning($message, array $context = []): void
     {
         $this->log(LogLevel::WARNING, $message, $context);
     }
 
-    public function notice(string $message, array $context = []): void
+    public function notice($message, array $context = []): void
     {
         $this->log(LogLevel::NOTICE, $message, $context);
     }
 
-    public function info(string $message, array $context = []): void
+    public function info($message, array $context = []): void
     {
         $this->log(LogLevel::INFO, $message, $context);
     }
 
-    public function debug(string $message, array $context = []): void
+    public function debug($message, array $context = []): void
     {
         if ($this->isDebug()) {
             $this->log(LogLevel::DEBUG, $message, $context);
         }
     }
 
-    public function log(string $level, $message, array $context = []): void
+    public function log($level, $message, array $context = []): void
     {
         $ouzoLevel = LogLevelTranslator::toSyslogLevel($level);
         $minimalLevel = $this->minimalLevels ? Arrays::getValue($this->minimalLevels, $this->name, LOG_DEBUG) : LOG_DEBUG;
